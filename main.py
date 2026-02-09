c7e26a0588d0833d27f4ecf6f6c0f92f52157996
Pranay <pranaykriplani@gmail.com>
2026-02-09 19:35:27 +0530

    Cleaned up the main file to start the app
    Simplified this file so its only job is to open the window. All the heavy work is now moved to other files.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import tkinter as tk
-from ui import FishyGuardUI
+from ui import PhishingUI
 
 if __name__ == "__main__":
     root = tk.Tk()
-    app = FishyGuardUI(root)
+    app = PhishingUI(root)
     root.mainloop()
