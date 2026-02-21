13a5cc215f2f3cfb09f9434ebcf875353f3af46c
Pranay <pranaykriplani@gmail.com>
2026-02-22 01:34:35 +0530

    Connect UI with Model and Database

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 import tkinter as tk
 from ui import PhishingUI
+from model import PhishingModel
+from database import PredictionHistory
+
+def main():
+    root = tk.Tk()
+    
+    # stttart logic and database first
+    model_engine = PhishingModel()
+    db = PredictionHistory()
+    
+    # Pass them into the UI
+    app = PhishingUI(root, model_engine, db)
+    
+    root.mainloop()
 
 if __name__ == "__main__":
-    root = tk.Tk()
-    app = PhishingUI(root)
-    root.mainloop()
+    main()
